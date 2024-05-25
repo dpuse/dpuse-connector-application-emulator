f90fbfe0679a97e416923e466b6603f696f7149f
Jonathan Terrell <terrell.jm@gmail.com>
2024-05-26 00:27:33 +0200

    v0.1.412

--- src/index.ts
+++ src/index.ts
@@ -144,8 +144,7 @@ const read = (connector: Connector, itemConfig: ItemConfig, previewConfig: DataV
                     let data;
                     while ((data = parser.read() as { info: CastingContext; record: string[] }) !== null) {
                         signal.throwIfAborted(); // Check if the abort signal has been triggered.
-                        console.log('FIELD INFOS', JSON.stringify(fieldInfos));
-                        pendingRows.push({ fieldInfos, fieldValues: data.record }); // Append the row of parsed values and associated information to the pending rows array.
+                        pendingRows.push({ fieldInfos: { ...fieldInfos }, fieldValues: data.record }); // Append the row of parsed values and associated information to the pending rows array.
                         if (pendingRows.length < DEFAULT_READ_CHUNK_SIZE) continue; // Continue with next iteration if the pending rows array is not yet full.
                         settings.chunk(pendingRows); // Pass the pending rows to the engine using the 'chunk' callback.
                         pendingRows = []; // Clear the pending rows array in preparation for the next batch of data.
