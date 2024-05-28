ba986e8046c312b461313b089ba2531810e598ef
Jonathan Terrell <terrell.jm@gmail.com>
2024-05-28 13:56:53 +0200

    v0.1.427

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ export default class ApplicationEmulatorConnector implements Connector {
         return { connector: this, read };
     }
 
-    async listItems(callback: (data: ConnectorCallbackData) => void, settings: ListSettings): Promise<ListResult> {
+    async list(callback: (data: ConnectorCallbackData) => void, settings: ListSettings): Promise<ListResult> {
         return new Promise((resolve, reject) => {
             try {
                 const indexItems = (applicationIndex as ApplicationIndex)[settings.folderPath];
