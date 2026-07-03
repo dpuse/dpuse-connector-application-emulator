9a17947b6b5439c3c296a0b669715437788344ef
Jonathan Terrell <terrell.jm@gmail.com>
2026-07-03 11:09:17 +0200

    v0.0.24

--- dist/dpuse-connector-application-emulator.es.js
+++ dist/dpuse-connector-application-emulator.es.js
@@ -170,7 +170,7 @@ var m = {
 	vendorAccountURL: null,
 	vendorDocumentationURL: null,
 	vendorHomeURL: null,
-	version: "0.0.22",
+	version: "0.0.24",
 	operations: [
 		"abortOperation",
 		"auditObjectContent",
